08eaddb3a26686ee29a6f82926b9135f2bca3a1e
nihclil <oxx921@gmail.com>
2023-11-06 15:50:34 +0800

    Add PlanForm to Route /plan

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,43 @@
-// import styled from "styled-components";
-// import Link from "next/link";
-// import { useState } from "react";
+"use client";
+import styled from "styled-components";
+import Link from "next/link";
+import { useState } from "react";
+import addButtonImage from "../public/icon-plus-circle.png";
 
 // export default function Home() {
 //   return <main></main>;
 // }
 
 export default function Home() {
-  return;
+  return (
+    <Main>
+      <AddArea>
+        <Link href="/plan">
+          <AddLink>
+            <PlusImg src="/icon-plus-circle.png"></PlusImg>
+            <AddSpan>Add a trip</AddSpan>
+          </AddLink>
+        </Link>
+      </AddArea>
+    </Main>
+  );
 }
+
+const Main = styled.main`
+  padding-top: 30px;
+  margin: auto;
+  width: 1200px;
+`;
+
+const AddArea = styled.div``;
+
+const AddLink = styled.div`
+  display: flex;
+`;
+
+const PlusImg = styled.img``;
+
+const AddSpan = styled.span`
+  cursor: pointer;
+  padding-left: 5px;
+`;
---
+++ components/PlanForm.jsx
@@ -0,0 +1,144 @@
+"use client";
+
+import styled from "styled-components";
+import Link from "next/link";
+
+export default function PlanForm() {
+  return (
+    <Main>
+      <AddArea>
+        <Title>Add trip</Title>
+
+        <PlanArea>
+          <TripInfo>
+            <Caption>
+              Add a trip manually below, and we&apos;ll create the trip for you.
+            </Caption>
+            <Column>
+              <InputName>Trip Name</InputName>
+              <Input></Input>
+            </Column>
+            <Column>
+              <InputName>Destination City</InputName>
+              <Input></Input>
+            </Column>
+            <DateColumn>
+              <Column>
+                <InputName>Start Date</InputName>
+                <DateInput></DateInput>
+              </Column>
+              <Column>
+                <InputName>End Date</InputName>
+                <DateInput></DateInput>
+              </Column>
+            </DateColumn>
+          </TripInfo>
+          <ImageInfo>
+            <Image></Image>
+            <ImageButton>Change Photo</ImageButton>
+          </ImageInfo>
+        </PlanArea>
+
+        <ConfirmArea>
+          <Link href="/">
+            <CancelButton>Cancel</CancelButton>
+          </Link>
+
+          <SaveButton>Save</SaveButton>
+        </ConfirmArea>
+      </AddArea>
+    </Main>
+  );
+}
+
+const Main = styled.main`
+  padding-top: 30px;
+  margin: auto;
+  width: 1200px;
+`;
+
+const AddArea = styled.div``;
+
+const Title = styled.div`
+  display: flex;
+  color: #283618;
+  font-size: 24px;
+  margin-bottom: 40px;
+`;
+
+const Caption = styled.div`
+  margin-bottom: 30px;
+`;
+
+const PlanArea = styled.div`
+  display: flex;
+`;
+
+const TripInfo = styled.div`
+  margin-right: 30px;
+`;
+
+const Column = styled.div`
+  margin-bottom: 40px;
+  margin-right: 10px;
+`;
+
+const InputName = styled.div`
+  margin-bottom: 10px;
+`;
+
+const Input = styled.input`
+  width: 600px;
+  height: 50px;
+  padding: 10px;
+  font-size: 20px;
+`;
+
+const DateInput = styled.input`
+  width: 295px;
+  height: 50px;
+  padding: 10px;
+  font-size: 20px;
+`;
+
+const DateColumn = styled.div`
+  display: flex;
+`;
+
+const ImageInfo = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+const Image = styled.div`
+  background-color: #264653;
+  width: 200px;
+  height: 200px;
+  margin-bottom: 30px;
+`;
+
+const ImageButton = styled.button`
+  border: 0;
+  background-color: transparent;
+  color: #e76f51;
+  font-weight: 600;
+`;
+
+const ConfirmArea = styled.div`
+  padding-top: 20px;
+`;
+
+const CancelButton = styled.button`
+  border: 0;
+  background-color: transparent;
+  margin-right: 40px;
+  font-size: 20px;
+  cursor: pointer;
+`;
+
+const SaveButton = styled.button`
+  border: 0;
+  background-color: transparent;
+  font-size: 20px;
+  cursor: pointer;
+`;
