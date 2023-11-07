b68f0a79515bb9403e84c84a81e3b568dce0db21
nihclil <oxx921@gmail.com>
2023-11-07 08:35:20 +0800

    Import AddTripBtn components

---
+++ app/trips/page.jsx
@@ -0,0 +1,8 @@
+"use client";
+import styled from "styled-components";
+import Link from "next/link";
+import AddTripBtn from "../../components/AddTripBtn";
+
+export default function Home() {
+  return <AddTripBtn />;
+}
