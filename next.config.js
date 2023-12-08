6966da05fb8b77891f8d37ec8443b1fee05cc969
nihclil <oxx921@gmail.com>
2023-12-09 03:22:45 +0800

    Add images config

--- next.config.js
+++ next.config.js
@@ -8,6 +8,18 @@ const nextConfig = {
         port: "",
         pathname: "/a/**",
       },
+      {
+        protocol: "https",
+        hostname: "firebasestorage.googleapis.com",
+        port: "",
+        pathname: "/v0/b/plannlog-a64d2.appspot.com/o/**",
+      },
+      {
+        protocol: "https",
+        hostname: "firebasestorage.googleapis.com",
+        port: "",
+        pathname: "/v0/b/plannlog-project-f1770.appspot.com/o/**",
+      },
     ],
   },
 };
