7785d3070c4ea07b3ed82a54d0e49d3d84b05da3
nihclil <oxx921@gmail.com>
2023-12-15 19:38:09 +0800

    Add CurrentPlansButton Component

---
+++ components/CurrentPlansButton.jsx
@@ -0,0 +1,41 @@
+import styled from "styled-components";
+import Image from "next/image";
+
+export default function CurrentPlansButton({ isOpen, onClick }) {
+  return (
+    <CurrentPlansButtonContainer onClick={onClick}>
+      <ButtonSpan>Current Plans</ButtonSpan>
+      <Image
+        src={
+          isOpen
+            ? "/iconmonstr-caret-down-circle-lined-24.png"
+            : "/iconmonstr-caret-up-circle-lined-24.png"
+        }
+        width={24}
+        height={24}
+        alt="currentPlans"
+      />
+    </CurrentPlansButtonContainer>
+  );
+}
+
+const CurrentPlansButtonContainer = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 250px;
+  background-color: #fff;
+  padding: 20px 20px;
+  border-radius: 20px;
+  cursor: pointer;
+`;
+
+const ButtonSpan = styled.span`
+  margin-right: 10px;
+  color: #6a9066;
+  font-size: 24px;
+  font-weight: 600;
+  &:hover {
+    color: #70946c;
+  }
+`;
