e64678803e8ad4e8fdd7e5de9a986c4f3425c99f
nihclil <oxx921@gmail.com>
2023-12-20 19:21:43 +0800

    Add PlanTypeButton component

---
+++ components/Common/Buttons/PlanTypeButton.jsx
@@ -0,0 +1,39 @@
+import styled from "styled-components";
+import Link from "next/link";
+import Image from "next/image";
+
+export default function PlanTypeButton({ linkHref, ImageSrc, ImageAlt, span }) {
+  return (
+    <PlanTypeButtonContainer>
+      <Link href={linkHref}>
+        <LinkContainer>
+          <Image src={ImageSrc} width={32} height={32} alt={ImageAlt} />
+          <PlanSpan>{span}</PlanSpan>
+        </LinkContainer>
+      </Link>
+    </PlanTypeButtonContainer>
+  );
+}
+
+const PlanTypeButtonContainer = styled.div`
+  border: 1px solid transparent;
+  background-color: #fff;
+  padding: 20px 30px;
+  border-radius: 40px;
+  color: #6d5b48;
+  transition: border 0.2s;
+  cursor: pointer;
+  &:hover {
+    border: 1px solid #6d5b48;
+  }
+`;
+
+const LinkContainer = styled.div`
+  display: flex;
+  align-items: center;
+`;
+
+const PlanSpan = styled.span`
+  margin-left: 10px;
+  font-size: 24px;
+`;
