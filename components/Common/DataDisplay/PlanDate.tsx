17cab5c0d26894562a0bb63aee23a7342bcd3e3b
nihclil <oxx921@gmail.com>
2023-12-28 18:51:53 +0800

    Convert from jsx to tsx

--- components/Common/DataDisplay/PlanDate.tsx
+++ components/Common/DataDisplay/PlanDate.tsx
@@ -1,7 +1,12 @@
 import styled from "styled-components";
 import formatDate from "utils/formatDate";
 
-export default function PlanDate({ date, option }) {
+interface Props {
+  date: string;
+  option: boolean;
+}
+
+export default function PlanDate({ date, option }: Props) {
   return <PlanDateContainer>{formatDate(date, option)}</PlanDateContainer>;
 }
 
